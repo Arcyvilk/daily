32eb04f1cedf46fea4ee4df3558e931c076979ed
Arcyvilk <arcyvilk@gmail.com>
2023-12-12 11:05:50 +0100

    refactor: 💡 change links to notion

--- src/pages/Plan/Plan.tsx
+++ src/pages/Plan/Plan.tsx
@@ -2,13 +2,13 @@ import { useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const days: Record<string, string> = {
-  mon: 'https://arcy.notion.site/Poniedzia-ek-305afd8c365341818813338019163a84?pvs=4',
-  tue: 'https://arcy.notion.site/Wtorek-19b12c04d0864b1fbc77b8d58f9d30ca?pvs=4',
-  wed: 'https://arcy.notion.site/roda-e944fbd6a15641ae849de8e9702cea2f?pvs=4',
+  mon: 'https://arcy.notion.site/Poniedzia-ek-bad27740cf4542288fd957d60730dc54?pvs=4',
+  tue: 'https://arcy.notion.site/Wtorek-f0f6902b2dea4812aaed15c255573e4b?pvs=4',
+  wed: 'https://arcy.notion.site/roda-2a25234a251d4f78a94639fe54934a95?pvs=4',
   thu: 'https://arcy.notion.site/Czwartek-fb780b474f634f03bc7126e69413f32f?pvs=4',
-  fri: 'https://arcy.notion.site/Pi-tek-842171c13bd0474cb54b2da3a98f76bb?pvs=4',
-  sat: 'https://arcy.notion.site/Sobota-c3dc4abd7b05478cb230fce2d7e71e4b?pvs=4',
-  sun: 'https://arcy.notion.site/Niedziela-3b1dcff065294019989ee6a68f67d855?pvs=4',
+  fri: 'https://arcy.notion.site/Pi-tek-d9dfe677976a40d6b54c747a777bdc37?pvs=4',
+  sat: 'https://arcy.notion.site/Sobota-6b49e5a5923c4c45ac06ac4abaec8cab?pvs=4',
+  sun: 'https://arcy.notion.site/Niedziela-009889ace2dd44fab893aa314ffc95fa?pvs=4',
 };
 
 export const Plan = (): JSX.Element => {
